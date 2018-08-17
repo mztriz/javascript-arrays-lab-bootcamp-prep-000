f7ee7ef063d0d85494906eaeddc1d1597702a479
Ava Barron <mztriz@gmail.com>
2018-08-17 19:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions and code here
+function Arrays(){
+  kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
